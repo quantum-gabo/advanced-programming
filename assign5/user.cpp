b90ae410102367df674d0ec92397926f02e3f8db
Gabriel Balarezo <gabrielbala2001@gmail.com>
2025-03-25 19:53:11 -0500

    assignment 5 complete

--- assign5/user.cpp
+++ assign5/user.cpp
@@ -63,3 +63,95 @@ void User::set_friend(size_t index, const std::string& name)
  * STUDENT TODO:
  * The definitions for your custom operators and special member functions will go here!
  */
+
+std::ostream& operator<< (std::ostream& os, const User& user) {
+    os << "User(name=" << user.get_name() << ", friends=[";
+    for (size_t i = 0; i < user.size(); ++i) {
+        os << user._friends[i];
+        if (i < user.size() - 1) {
+            os << ", ";
+        }
+    }
+    os << "])";
+    return os;
+}
+
+/* 
+    * Destructor for the User class.
+*/
+User::~User() {
+    delete[] _friends; // Safe: matches new[]
+}
+/*
+    * Copy constructor for the User class.
+*/
+User::User(const User& user) 
+    : _name(user._name), _size(user._size), _capacity(user._capacity) {
+    _friends = new std::string[_capacity];
+    for (size_t i = 0; i < _size; ++i) {
+        _friends[i] = user._friends[i];
+    }
+}
+
+/*
+Copy assignment operator for the User class.
+*/
+User& User::operator=(const User& user) {
+    if (this == &user) {
+        return *this; // Handle self-assignment
+    }
+    // Allocate new memory first
+    std::string* newFriends = new std::string[user._capacity];
+
+    // Copy the data from the user object
+    for (size_t i = 0; i < user._size; ++i) {
+        newFriends[i] = user._friends[i];
+    }
+    // Delete the old memory
+    delete[] _friends;
+
+    // Copy the data from the user object
+    _name = user._name;
+    _size = user._size;
+    _capacity = user._capacity;
+    _friends = newFriends;
+
+    return *this;
+}
+
+/*
+    * Operator += for adding a friend to the User class.
+    must be symmetric (i.e., if A += B, then automatically B += A)
+*/
+User& User::operator+=(User& new_friend) {
+    // Check if the new friend is the same as this user
+    if (this == &new_friend) {
+        return *this; // No self-friendship
+    }
+    // Check if the friend is already in the list
+    for (size_t i = 0; i < _size; ++i) {
+        if (_friends[i] == new_friend.get_name() && &new_friend != this) {
+            return *this; // Friend already exists, no need to add
+        }
+    }
+
+    // Check if this user is already in the new friend's friend list
+    for (size_t i = 0; i < new_friend._size; ++i) {
+        if (new_friend._friends[i] == _name && &new_friend != this) {
+            return *this; // Already mutual friends
+        }
+    }
+
+    // Add the new friend
+    add_friend(new_friend.get_name());
+    new_friend.add_friend(_name); // Add this user as a friend to the new friend
+
+    return *this;
+}
+
+/*
+    * Operator < for comparing two users.
+*/
+bool User::operator<(const User& rhs) const {
+    return _name < rhs._name; // Compare based on names
+}
--- assign5/user.h
+++ assign5/user.h
@@ -19,6 +19,29 @@ public:
    * STUDENT TODO:
    * Your custom operators and special member functions will go here!
    */
+  friend std::ostream& operator<<(std::ostream& os, const User& user);
+
+  // Destructor
+    ~User();
+
+    // Copy constructor
+    User(const User& user);
+
+    // Copy assignment operator
+    User& operator=(const User& user);
+
+    // Move constructor
+    User(User&& user) = delete; // Disable move constructor
+
+    // Move assignment operator
+    User& operator=(User&& user) = delete; // Disable move assignment
+
+    // Operator += for adding a friend
+    User& operator+=(User& new_friend);
+
+    // Operator < for comparing two users
+    bool operator<(const User& rhs) const;
+
 
 private:
   std::string _name;
